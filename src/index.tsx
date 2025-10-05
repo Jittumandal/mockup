989690ad7f992ebd0451f85f3e0fab15e0d084d0
Jittumandal <jitendrakumar.designer@gmail.com>
2025-10-05 19:39:51 +0530

    add page and approutes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { StyledEngineProvider } from "@mui/material/styles";
+import App from "./App";
 
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import App from './MarketingPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <App />
